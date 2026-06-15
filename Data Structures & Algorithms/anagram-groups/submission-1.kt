c870ae47bfe382f9f5f3bb885e280ffafa64add4
Alok kumar Singh <aloksingh191196@gmail.com>
2026-06-15 08:52:38 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+         val finalHashmap = HashMap<HashMap<Char, Int>, MutableList<String>>()
+
+        for (anagram in strs) {
+            val anagramMap = anagramHashmap(anagram)
+            if (!finalHashmap.containsKey(anagramMap)) {
+                finalHashmap[anagramMap] = mutableListOf()
+            }
+            finalHashmap[anagramMap]?.add(anagram)
+        }
+
+        return finalHashmap.values.toList()
+    }
+
+
+    fun anagramHashmap(str: String): HashMap<Char, Int> {
+        val sMap = HashMap<Char, Int>()
+        for (s in str) {
+            sMap[s] = sMap.getOrDefault(s, 0) + 1
+        }
+        return sMap
+    }
+
+
+}
