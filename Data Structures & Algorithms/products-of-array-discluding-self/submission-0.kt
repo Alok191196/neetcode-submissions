8e94d1c5bc2105f3f225a1b9cb1dfa39a4a564d2
Alok kumar Singh <aloksingh191196@gmail.com>
2026-06-15 09:58:20 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val prefixMul = IntArray(nums.size)
+
+        val n = nums.size
+        val res = IntArray(n)
+
+        for (i in 0 until n) {
+            var prod = 1
+            for (j in 0 until n) {
+                if (i == j) continue
+                prod *= nums[j]
+            }
+            res[i] = prod
+        }
+        return res
+    }
+}
