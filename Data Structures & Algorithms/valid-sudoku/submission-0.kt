78f1844a6449888aafd6a2df9366335e8c54f169
Alok kumar Singh <aloksingh191196@gmail.com>
2026-06-22 16:24:22 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.kt
@@ -0,0 +1,63 @@
+class Solution {
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+        // check row
+        for (arr in board) {
+            if (!isValidRC(arr)) {
+                println("1" + arr.toList())
+                return false
+            }
+        }
+
+        // check Column
+        for (i in 0 .. 8) {
+            var temp = CharArray(9)
+            for (j in 0 .. 8) {
+                temp[j] = board[j][i]
+            }
+            if (!isValidRC(temp)) {
+                println("2" + temp)
+                return false
+            }
+            temp = CharArray(9)
+        } 
+
+        // check Box
+        for (i in 0 .. 8 step 3) {
+            // box start position (0,0) (0,3) (0,6)
+            // (3,0) (3,3) (3,6)
+            // (6,0) (6,3) (6,6)
+            for (j in 0..8 step 3) {
+                val tempArr = getTempSubBox(i, j, board)
+                if (!isValidRC(tempArr)) {
+                    println("3 i = $i j = $j" + tempArr.toList())
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
+    fun getTempSubBox(row: Int, col: Int, board: Array<CharArray>) : CharArray {
+        val temp = CharArray(9)
+        var counter = 0
+        for (i in row .. row + 2) {
+            for (j in col .. col + 2) {
+                temp[counter] = board[i][j]
+                counter++
+            }
+        }
+        return temp
+    }
+
+    fun isValidRC(arrs: CharArray) : Boolean {
+        val map = HashMap<Char, Int>()
+        for (arr in arrs) {
+            if (arr != '.') {
+                map[arr] = map.getOrDefault(arr, 0) + 1
+            }
+        }
+        return map.values.none { it > 1 }
+    }
+
+}
