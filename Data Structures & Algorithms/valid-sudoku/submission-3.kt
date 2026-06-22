809d7544e328495d5f775bdb6d6363c7d4fcde83
Alok kumar Singh <aloksingh191196@gmail.com>
2026-06-22 16:40:36 +0530

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+        val rows = Array(9) { HashSet<Char>() }
+        val cols = Array(9) { HashSet<Char>() }
+        val squares = Array(9) { HashSet<Char>() }
+
+
+        for ( r in 0 until 9) {
+            for ( c in 0 until 9) {
+                val value = board[r][c]
+                if (value == '.') continue
+            
+                val sqId = (r/3)*3+(c/3)
+
+                if (value in rows[r] || value in cols[c] || value in squares[sqId]) return false
+
+                rows[r].add(value)
+                cols[c].add(value)
+                squares[sqId].add(value)
+            }
+        }
+        return true
+    }
+}
